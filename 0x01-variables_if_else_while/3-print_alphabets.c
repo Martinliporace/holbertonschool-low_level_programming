06846165be6f069705f8799453233e7c47320ac5
Martinliporace <johnd.com>
2022-02-04 11:58:16 -0800

    print alphabets fixed style

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,8 +13,8 @@ char letra;
 for (letra = 'a'; letra <= 'z'; letra++)
 putchar(letra);
 
-   for (letra = 'A'; letra <= 'Z'; letra++)
-   putchar(letra);
+for (letra = 'A'; letra <= 'Z'; letra++)
+putchar(letra);
 
 putchar('\n');
 
