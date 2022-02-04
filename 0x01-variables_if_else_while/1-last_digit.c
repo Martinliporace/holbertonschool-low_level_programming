c9338730d3bb8b9a272cb49cc63519d03bb341d0
Martinliporace <johnd.com>
2022-02-04 08:01:14 -0800

    last digit 2.3

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
 /*
 *main - prints a random number, displays the last digit and indicates
 *if it is greater or less than 5 or if it is 0.
