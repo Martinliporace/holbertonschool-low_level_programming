9a65011f9ceee7b6c9bc409b2398aa03abc1bb82
Martinliporace <johnd.com>
2022-02-04 16:13:30 -0800

    print numbers 2

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,25 +1,18 @@
-/*
- * File: 4-print_alphabt.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
 
 /**
-* main - prints all single digit numbers of base 10 starting from 0
+* main - Prints all single digit numbers of base 10 starting from 0.
 *
 * Return: Always 0.
 */
 int main(void)
 {
-char num;
+int num;
 
-for (num = '0'; num <= '9'; num++)
-{	
-putchar(num);
-}
+for (num = 0; num < 10; num++)
+printf("%d", num);
 
-putchar('\n');
+printf("\n");
 
 return (0);
 }
