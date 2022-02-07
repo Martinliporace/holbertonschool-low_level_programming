7124305004298f51ffa611fe437891f1c7b173f0
Martinliporace <johnd.com>
2022-02-07 07:44:46 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* main - Prints the alphabet in lowercase.
+*
+* Return: Always 0.
+*
+*/
+
+
+void print_alphabet(void)
+{
+char letra;
+
+for (letra = 'a'; letra <= 'z'; letra++)
+_putchar(letra);
+
+_putchar('\n');
+
+return (0);
+}
