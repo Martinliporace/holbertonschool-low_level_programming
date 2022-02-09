9c3cc59d20f09ec3e4e9381a828f17da5a4394cf
Martinliporace <johnd.com>
2022-02-09 13:42:09 -0800

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,25 @@ int main(void)
 {
 int n;
 for (n = 1; n <= 100; n++)
+{
 if ((n % 3 == 0) && (n % 5 == 0))
 {
 printf("FizzBuzz ");
 }
 else if (n % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else if (n % 5 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else
 {
 printf("%d", n);
+}
+if (n == 100)
+continue;
 printf(" ");
 }
 return (0);
