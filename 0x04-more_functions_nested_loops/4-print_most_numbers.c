145f9abbe6d48e7abf3be3934831d2f91b8dd5e4
Martinliporace <johnd.com>
2022-02-09 10:29:28 -0800

    print more numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* print_more_numbers -  prints the numbers, from 0 to 9
-*
+* print_most_numbers - prints the numbers, from 0 to 9
+* Description: omite 4 and 2
 * Return: numbers 1..9 except 2 and 4
 *
 */
@@ -12,6 +12,7 @@ void print_most_numbers(void)
 int num;
 
 for (num = '0'; num <= '9'; num++)
+
 if (num != '2' && num != '4')
 _putchar(num);
 
