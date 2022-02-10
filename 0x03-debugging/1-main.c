d2bbb681836dd1f1a061d17c2ffc97c45fb4e58c
Martinliporace <johnd.com>
2022-02-10 04:31:08 -0800

    1 main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,11 +8,8 @@
 int main(void)
 {
 int i;
-
 printf("Infinite loop incoming :(\n");
-
 i = 0;
-
 /**
 *while (i < 10)
 *{
@@ -24,4 +21,3 @@ printf("Infinite loop avoided! \\o/\n");
 
 return (0);
 }
-
