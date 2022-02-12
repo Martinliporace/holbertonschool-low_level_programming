7b8cc8504c18cfb1644ba19b5b986b370256f99d
Martinliporace <johnd.com>
2022-02-12 09:13:00 -0800

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,22 +7,29 @@
 * Return: Always 0 (Success)
 */
 
+
 void puts_half(char *str)
 {
-int t, tf, m, len;
-len = _strlen(str);
-t = len;
-tf = (t % 2);
-m = 0;
-if (tf == 0)
-m = (t/2);
-else
-m = ((t - 1) / 2);
-while (*(str + m))
+int len = 0;
+char *t = str;
+int n;
+
+while (*t != 0)
 {
-_putchar(*(str + m));
-m++;
+t++;
+len++;
+}
+if (len % 2 == 0)
+{
+n = len / 2;
+}
+else
+{
+n = (len + 1) / 2;
+}
+for ( ; n < len ; n++)
+{
+_putchar(str[n]);
 }
 _putchar('\n');
 }
-      
