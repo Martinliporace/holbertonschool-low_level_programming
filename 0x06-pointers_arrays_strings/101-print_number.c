b9531d8d7dcb217893d08fa35e3b6beca0aa2733
Martinliporace <johnd.com>
2022-02-16 11:42:01 -0800

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,25 +1,21 @@
 #include "main.h"
 
 /**
-* print_number - Prints an integer.
-* @n: number
-* Description: coso
-*/
+ * print_number - Prints an integer.
+ * @n: number
+ */
 void print_number(int n)
-
 {
-	int num;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -n;
+		num = -num;
 	}
-	else
-		num = n;
-	if (num / 10 != 0)
-	{
+
+	if ((num / 10) > 0)
 		print_number(num / 10);
-	}
-	_putchar('0' + (num % 10));
+
+	_putchar((num % 10) + '0');
 }
