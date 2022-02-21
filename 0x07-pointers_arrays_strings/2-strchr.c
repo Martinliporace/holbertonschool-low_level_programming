b995f7ee5656093d85570c2045bb9959bf4cc41f
Martinliporace <johnd.com>
2022-02-21 06:03:30 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strchr - locates a character in a string.
+* @s: coso
+* @c: pum
+*
+* Description: show a string
+* Return: a pointer to the first occurrence of the character c
+*/
+
+char *_strchr(char *s, char c)
+{
+int cont = 0;
+
+while (*(s + cont))
+{
+if (*(s + cont) == c)
+return (s + cont);
+cont++;
+}
+return (0);
+}
