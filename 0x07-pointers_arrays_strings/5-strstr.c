cfc0119b91756f34fe1288fe457fd313085cc86d
Martinliporace <johnd.com>
2022-02-21 06:14:07 -0800

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strstr - locates a substring in a string.
+* @haystack: coso
+* @needle: pum
+*
+* Description: show a string
+* Return: pointer to the beginning of the located substring
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+int cont = 0;
+
+while (*(haystack + cont))
+{
+if (*(haystack + cont) == *needle)
+return (haystack + cont);
+cont++;
+}
+return (0);
+}
