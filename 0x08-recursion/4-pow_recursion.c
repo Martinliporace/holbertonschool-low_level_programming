af7c8413c8232d10136d11778135f8bc5c34deb6
Martinliporace <johnd.com>
2022-02-23 14:10:05 -0800

    pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* int _pow_recursion - returns the value of x raised to the power of y.
+*
+* @x: coso
+* @y: pum
+* Return: x raised to the pooooweeeeer of y
+**/
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+	return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
