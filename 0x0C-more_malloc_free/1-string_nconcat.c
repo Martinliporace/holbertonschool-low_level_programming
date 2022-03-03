c6474014977165e06b9e47fea1c907fb95d93525
Martinliporace <johnd.com>
2022-03-03 04:35:58 -0800

    string n concat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* string_nconcat - concatenates two strings.
+* @s1: coso
+* @s2: pum
+* @n: ahre
+*
+* Return: pointer of an array of chars or NULL
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+	char *copia;
+
+	unsigned int i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0' && j <= n)
+		j++;
+
+	copia = malloc(sizeof(char) * (i + j + 1));
+
+	if (copia == NULL)
+		return (NULL);
+	i = j = 0;
+
+	while (s1[i] != '\0')
+	{
+		copia[i] = s1[i];
+		i++;
+	}
+
+	while (s2[j] != '\0' && j <= n)
+	{
+		copia[i] = s2[j];
+		i++, j++;
+	}
+
+	copia[i] = '\0';
+	return (copia);
+}
