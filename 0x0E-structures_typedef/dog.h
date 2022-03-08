d65fd8203929050a2211d960f64acd8365e7f254
Martinliporace <johnd.com>
2022-03-08 05:52:17 -0800

    dog h with all the prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,8 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,3 +12,11 @@ char *name;
 float age;
 char *owner;
 };
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
