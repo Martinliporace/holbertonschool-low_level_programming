f1b5d74ac699e908bd156242d08d73145223057c
Martinliporace <johnd.com>
2022-03-09 12:32:03 -0800

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,14 @@
 #include "function_pointers.h"
 
 /**
-* print_name - print a name
+* print_name - prints a name
 * @name: coso
 * @f: pum
-* Return: void
+*
+* Return: ahre
 */
 
 void print_name(char *name, void (*f)(char *name))
-
 {
 if (name != NULL && f != NULL)
 f(name);
