def3b7440152f6418781a2be341ff1e8160c7551
Martinliporace <johnd.com>
2022-03-09 12:20:32 -0800

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,11 @@
 * print_name - print a name
 * @name: coso
 * @f: pum
-* Return: ahre
+* Return: void
 */
 
 void print_name(char *name, void (*f)(char *name))
+
 {
 if (name != NULL && f != NULL)
 f(name);
