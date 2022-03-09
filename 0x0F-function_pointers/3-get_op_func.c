0264208c20ad6383f2711815f0c8a2b4f8ba1bf8
Martinliporace <johnd.com>
2022-03-09 14:19:04 -0800

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,13 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
 #include "function_pointers.h"
 
 /**
- * get_op_func - select function
- * @s: array of elements
- *
- *
- * Return: coso
- */
+* get_op_func - selects the correct function 
+* to perform the operation 
+* @s: array of elements
+*
+* Description: coso
+* Return: pum
+*/
 
 int (*get_op_func(char *s))(int, int)
 {
