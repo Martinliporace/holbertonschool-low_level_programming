724fa5a2bdffb43cc69cd91882926cb716071778
Martinliporace <johnd.com>
2022-03-09 15:15:02 -0800

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,31 +1,31 @@
 #include "3-calc.h"
-#include <stddef.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * get_op_func - coso
  * @s: pum
  * Return: ahre
  */
-int (*get_op_func(char *s))(int a, int b)
+
+int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i;
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+};
+	int i = 0;
 
-	i = 0;
-	while (i < 5)
+	while (ops[i].op != NULL)
 	{
-		if (ops[i].op[0] == s[0])
+		if (*ops[i].op == *s)
 		{
 			return (ops[i].f);
 		}
 		i++;
 	}
-	return (NULL);
+	return (0);
 }
