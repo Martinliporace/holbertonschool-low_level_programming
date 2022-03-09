c5b55620adbf97c766e2544c94e019562f39b760
Martinliporace <johnd.com>
2022-03-09 14:26:23 -0800

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,43 +1,34 @@
 #include "3-calc.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
 * main - check the code
 * @argc: coso
 * @argv: pum
 *
-* Return: Always 0.
+* Description: check the code
+* Return: ahre
 */
 
 int main(int argc, char *argv[])
-
 {
-	int a, b;
-	int (*operation)(int, int);
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	if (argv[2][1])
+	if ((argv[2][0] != 37 && argv[2][0] != 47 && argv[2][0] != 43 &&
+	argv[2][0] != 45 && argv[2][0] != 42) || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	operation = get_op_func(argv[2]);
-
-	if (operation == NULL)
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-
-	printf("%d\n", operation(a, b));
+	printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
