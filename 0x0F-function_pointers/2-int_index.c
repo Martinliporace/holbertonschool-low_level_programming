f4caf8d896bd7939f2e1df729ef31e1a3600b71a
Martinliporace <johnd.com>
2022-03-09 12:30:01 -0800

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,24 +9,16 @@
 */
 
 int int_index(int *array, int size, int (*cmp)(int))
-
 {
+	int i;
 
-{
-if (size <= 0)
-	return (-1);
-}
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
 
-{
-int i;
-int x = size;
-
-for (i = 0; i <= x; i++)
-
-
-if (cmp(array[i]) != 0)
-return (i);
-}
-
-return (-1);
+	}
+		return (-1);
 }
