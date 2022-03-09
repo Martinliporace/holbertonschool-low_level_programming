eb8ce9567cac27b278098ca482076f10e0bf50f9
Martinliporace <johnd.com>
2022-03-09 14:29:02 -0800

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,74 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "3-calc.h"
 
 /**
-* op_add - adds two ints
-* @a: number 1
-* @b: number 2
-* Return: the sum of a and b
-**/
+* op_add - add
+* @a: array of elements
+* @b: number
+*
+* Return: coso
+*/
 
 int op_add(int a, int b)
-
 {
 	return (a + b);
 }
 
-
 /**
-* op_sub - subtract two numbers
-* @a: number 1
-* @b: number 2
-* Return: the difference of a and b
-**/
+* op_sub - substract
+* @a: array of elements
+* @b: number
+*
+*
+* Return: coso
+*/
 
 int op_sub(int a, int b)
-
 {
 	return (a - b);
 }
 
-
 /**
-* op_mul - multiply two numbers
-* @a: number 1
-* @b: number 2
-* Return: the product of a and b
-**/
+* op_mul - multiplicate
+* @a: array of elements
+* @b: number
+*
+*
+* Return: coso
+*/
 
 int op_mul(int a, int b)
-
 {
 	return (a * b);
 }
 
-
 /**
-* op_div - divides two numbers
-* @a: number 1
-* @b: number 2
-* Return: the result of the division of a by b
-**/
+* op_div - division
+* @a: array of elements
+* @b: number
+*
+*
+* Return: coso
+*/
 
 int op_div(int a, int b)
-
 {
 	if (b == 0)
-		return (-1);
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
-
 /**
-* op_mod - calculates mod of two ints
-* @a: number 1
-* @b: number 2
-* Return: the remainder of the division of a by b
-**/
+* op_mod - modulo
+* @a: array of elements
+* @b: number
+*
+*
+* Return: coso
+*/
 
 int op_mod(int a, int b)
-
 {
 	if (b == 0)
-		return (-1);
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
+
