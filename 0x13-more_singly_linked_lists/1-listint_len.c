546e4006cfecb46fb77a0759484926ff31ef3b65
Martinliporace <johnd.com>
2022-03-22 07:19:30 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* listint_len - coso
+* @h: pum
+* Return: ahre
+*/
+
+
+
+
+size_t listint_len(const listint_t *h)
+
+{
+	size_t i = 1;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	while (h->next != NULL)
+	{
+		
+		i++;
+		h = h->next;
+	}
+
+	
+	return (i);
+}
