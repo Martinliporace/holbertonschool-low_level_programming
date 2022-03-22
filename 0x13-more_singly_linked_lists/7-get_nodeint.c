0c753697449459a861c2b70088259393977cb3bd
Martinliporace <johnd.com>
2022-03-22 16:32:15 -0700

    7-get_nodeint.c fxd

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int cont = 0;
 	listint_t *aux = head;
 
-	while (head)
+	while (aux)
 	{
 		if (cont == index)
 			return (aux);
