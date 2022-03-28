c949e9688b88a1bca937bbd40f8c7af39cfe3260
Martinliporace <johnd.com>
2022-03-28 16:10:00 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+* create_file - creates a file
+* @filename: filename.
+*
+* @text_content: text in the file.
+*
+* Return: 1 on success, -1 on failure 
+*/
+
+
+int create_file(const char *filename, char *text_content)
+
+{
+	int name;
+	int size;
+	int rwrite;
+
+	if (!filename)
+		return (-1);
+
+	name = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (name == -1)
+		return (-1);
+
+	if (!text_content)
+		return (1);
+
+	for (size = 0; text_content[size]; size++)
+		;
+
+	rwrite = write(name, text_content, size);
+
+	if (rwrite == -1)
+		return (-1);
+
+	close(name);
+
+	return (1);
+}
