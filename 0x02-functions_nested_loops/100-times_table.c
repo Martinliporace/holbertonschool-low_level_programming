1779868a555c1a40d83073a2c707f44641ac8807
root <martin.liporace@gmail.com>
2022-04-12 16:37:14 -0700

    100-times_table betty fixed

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,35 +1,28 @@
 #include "main.h"
 
-
 /**
 * print_times_table - If n > 15 or < 0 the function should not print anything
 * Format: see example
-*
-* a - number 1, b - number 2, r - result
-*
+* @n: number
 * Return: times table
 **/
 
 void print_times_table(int n)
-
 
 {
 int a, b, r;
 
 	if (n >= 0 && n < 15)
 
-
 	for (a = 0; a <= n; a++)
-
 
 	{	_putchar(48);
 		for (b = 1; b <= n; b++)
 		{
 		r = (a * b);
-		
 		_putchar(',');
 		_putchar(' ');
-		
+
 		if ((r / 100) > 0) /* r > 99 */
 		{
 		_putchar((r / 100) + '0');
@@ -38,26 +31,20 @@ int a, b, r;
 		}
 
 		else if ((r / 10) > 0) /* r > 9 */
-
 		{
 		_putchar(' ');
 		_putchar((r / 10) + '0');
 		_putchar((r % 10) + '0');
 		}
-		
-		else /* r <= 9 */
 
+		else /* r <= 9 */
 		{
 		_putchar(' ');
 		_putchar(' ');
 		_putchar(r + '0');
 		}
-	
-
-
 	}
 	_putchar('\n');
 	}
-
 }
 
