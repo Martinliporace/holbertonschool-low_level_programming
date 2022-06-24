38680e549905b59053a345c2f76b9630243ff74b
root <martin.liporace@gmail.com>
2022-06-24 06:38:25 -0700

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+
+{
+return(malloc(size));
+}
