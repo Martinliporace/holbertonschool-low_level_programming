ac9a4310bebb600111e453611df2645aa1bc6d48
root <martin.liporace@gmail.com>
2022-06-24 08:43:00 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,16 +11,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *ht;
 if (size < 1)
-	return NULL;
+	return (NULL);
 ht = malloc(sizeof(ht));
 if (ht == NULL)
 return (NULL);
 
-ht->array = (hash_node_t**)malloc(size *sizeof(hash_node_t));
+ht->array = (hash_node_t **) malloc(size * sizeof(hash_node_t));
 if (ht->array == NULL)
 return (NULL);
-memset(ht->array, 0, size *sizeof(hash_node_t));
+memset(ht->array, 0, size * sizeof(hash_node_t));
 ht->size = size;
-return ht;
+return (ht);
 }
 
