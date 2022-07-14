ae8628f3999d2ca5b6d683b3a5f976b64db3a387
root <martin.liporace@gmail.com>
2022-07-14 11:19:24 -0700

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,25 +6,24 @@ def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
 
     per = 0
+    if grid:
+        for x in range(len(grid)):
+            for y in range(len(grid[x])):
+                if grid[x][y] == 1:
+                    sum = 0
+                    up = grid[x-1][y]
+                    down = grid[x+1][y]
+                    left = grid[x][y-1]
+                    right = grid[x][y+1]
+                    if up == 1:
+                        sum += 1
+                    if down == 1:
+                        sum += 1
+                    if left == 1:
+                        sum += 1
+                    if right == 1:
+                        sum += 1
 
-    for x in range(len(grid)):
-        for y in range(len(grid[x])):
-            if grid[x][y] == 1:
-                sum = 0
-                square = grid[x][y]
-                up = grid[x-1][y]
-                down = grid[x+1][y]
-                left = grid[x][y-1]
-                right = grid[x][y+1]
-                if up == 1:
-                    sum += 1
-                if down == 1:
-                    sum += 1
-                if left == 1:
-                    sum += 1
-                if right == 1:
-                    sum += 1
-
-                tmp = 4 - sum
-                per += tmp
+                    tmp = 4 - sum
+                    per += tmp
     return(per)
