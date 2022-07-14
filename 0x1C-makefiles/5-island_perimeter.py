20b54e6550ef82b3d413b4529d12e622c305ec7d
root <martin.liporace@gmail.com>
2022-07-14 11:09:21 -0700

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,26 +8,21 @@ def island_perimeter(grid):
     for x in range(len(grid)):
         for y in range(len(grid[x])):
             if grid[x][y] == 1:
-                tmp = 4 - check_for_land(grid, x, y)
+                sum = 0
+                square = grid[x][y]
+                up = grid[x-1][y]
+                down = grid[x+1][y]
+                left = grid[x][y-1]
+                right = grid[x][y+1]
+                if up == 1:
+                    sum += 1
+                if down == 1:
+                    sum += 1
+                if left == 1:
+                    sum += 1
+                if right == 1:
+                    sum += 1
+
+                tmp = 4 - sum
                 per += tmp
     return(per)
-
-
-def check_for_land(grid, x, y):
-    """check for neighbors squares"""
-
-    sum = 0
-    square = grid[x][y]
-    up = grid[x-1][y]
-    down = grid[x+1][y]
-    left = grid[x][y-1]
-    right = grid[x][y+1]
-    if up == 1:
-        sum += 1
-    if down == 1:
-        sum += 1
-    if left == 1:
-        sum += 1
-    if right == 1:
-        sum += 1
-    return(sum)
